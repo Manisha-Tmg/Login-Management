3a26f6b89ca955429def73b7a00245c0f0c1e68c
Manisha Tamang <manishatamang752@gmail.com>
2026-01-28 10:00:00 +0545

    Routes feat added

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,10 @@
 import cors from "cors";
 import express, { json } from "express";
 import connectToDb from "./src/connectToDb/connectToDb.js";
+import productRouter from "./src/Routes/productRouter.js";
+import reviewRouter from "./src/Routes/reviewRouter.js";
+import fileRouter from "./src/Routes/fileRouter.js";
+import webUserRouter from "./src/Routes/webUserRouter.js";
 let app = express();
 app.use(cors());
 let port = 8000;
@@ -15,3 +19,9 @@ app.listen(port, () => {
 });
 
 app.use(json()); //make our system capable to take json data , always place it at top
+
+app.use(express.static("public"));
+app.use("/products", productRouter);
+app.use("/reviews", reviewRouter);
+app.use("/file", fileRouter);
+app.use("/web-users", webUserRouter);
