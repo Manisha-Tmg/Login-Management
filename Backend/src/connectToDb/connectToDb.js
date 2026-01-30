14ae4d8d3788bcf02d372800cd733a474acc8293
Manisha Tamang <manishatamang752@gmail.com>
2026-01-30 10:00:00 +0545

    error fixed

--- Backend/src/connectToDb/connectToDb.js
+++ Backend/src/connectToDb/connectToDb.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import { dbUrl } from "../config.js";
 
 const connectToDb = async () => {
-  await mongoose.connect("mongodb://0.0.0.0:27017/dw26666");
+  await mongoose.connect(dbUrl);
   console.log("Application is connected to database sucessfully");
 };
 
